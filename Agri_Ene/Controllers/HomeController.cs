911174e9db4e46c2237f57405dea8fb665564031
ST10158643 <ST10158643@vcconnect.edu.za>
2024-05-28 05:07:35 +0200

    With Navigation to the Farmer and Employee Index

--- Agri_Ene/Controllers/HomeController.cs
+++ Agri_Ene/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Agri_Ene.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,14 @@ namespace Agri_Ene.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AgriUser> _userManager;
+        private readonly SignInManager<AgriUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AgriUser> userManager, SignInManager<AgriUser> signInManager)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -27,6 +32,32 @@ namespace Agri_Ene.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetUserRole()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Contains("farmer"))
+                {
+                    return RedirectToAction("Index", "Account"); // Redirect to Account/Index for farmers
+                }
+                else if (roles.Contains("employee"))
+                {
+                    return RedirectToAction("Index", "Employee"); // Assuming you have a specific view for employees
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home"); // Default action if role is not matched
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home"); // Redirect to home if user is not signed in
+            }
         }
     }
 }
